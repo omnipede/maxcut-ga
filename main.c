fc6e0da16a8f5422d7246b0c24d43634ca970352
omnipede <omnipede@naver.com>
2022-05-05 11:33:49 +0900

    기능: mutation method 변경
    교과서와 유사한 방법으로 uniform mutation 을 구현함

--- main.c
+++ main.c
@@ -90,8 +90,13 @@ int main(int argc, char *argv[]) {
         }
 
         // Mutation
-        int mutated_index = (int) (rand() / ((double)RAND_MAX + 1.0) * num_of_vertex) % num_of_vertex;
-        child[mutated_index] = !child[mutated_index];
+        // For each bit
+        for (int i = 0; i < num_of_vertex; ++i) {
+            double r = rand() / ((double) RAND_MAX + 1.0);
+            // Flip a bit in 2% probability
+            if (r < 0.02)
+                child[i] = !child[i];
+        }
 
         // Replace with worst case
         for (int i = 0; i < num_of_vertex; ++i)
